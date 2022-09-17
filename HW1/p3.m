dbbfccd96b5260d76e5c1e4c36fce9631300a0b4
suuuuu017 <suuuuu0177@outlook.com>
2022-09-17 19:13:05 -0400

    add different goals

--- HW1/p3.m
+++ HW1/p3.m
@@ -38,8 +38,8 @@ setpoint = 3;
 i = 1;
 
 Kp = 1.5;
-Ki = 0;
-Kd = 0;
+Ki = 0.1;
+Kd = 0.001;
 
 dt = 0.1;
 integral = 0;
@@ -54,34 +54,35 @@ time = [];
 xData = [];
 vel = 0;
 
-while (xp < destX(destIdx) - 0.1) && (yp < destY(destIdx) - 0.1)
-    vd = sqrt((destX(destIdx) - xp)^2 + (destY(destIdx) - yp)^2);
-    measured_value = vel;
-
-    error = setpoint - measured_value;
-    integral = integral + error*dt;
-    derivative = (error - previous_error)/dt ;
-    output = Kp*error + Ki*integral + Kd*derivative;
-    vel = 0.99*vel + output;
-    velData = [velData, vel];
-    previous_error = error;
-
-    theta = atan2(destY(destIdx) - yp, destX(destIdx) - xp);
-    xp = xp + vel * cos(theta) * dt;
-    xData = [xData, xp];
-    yp = yp + vel * sin(theta) * dt;
-
-    i = i + 1;
-    time = [time, i];
-
-    robot = TriangularRobot(xp,yp,theta(1));
-    plot(robot(:,1),robot(:,2),'-',xp,yp,'-');
-    xlim([0 200])
-    ylim([0 200])
-    pause(0.01)
+for destIdx = 1:3
+    while (xp < destX(destIdx) - 0.1) && (yp < destY(destIdx) - 0.1)
+        vd = sqrt((destX(destIdx) - xp)^2 + (destY(destIdx) - yp)^2);
+        measured_value = vel;
     
-end    
-
+        error = setpoint - measured_value;
+        integral = integral + error*dt;
+        derivative = (error - previous_error)/dt ;
+        output = Kp*error + Ki*integral + Kd*derivative;
+        vel = 0.99*vel + output;
+        velData = [velData, vel];
+        previous_error = error;
+    
+        theta = atan2(destY(destIdx) - yp, destX(destIdx) - xp);
+        xp = xp + vel * cos(theta) * dt;
+        xData = [xData, xp];
+        yp = yp + vel * sin(theta) * dt;
+    
+        i = i + 1;
+        time = [time, i];
+    
+        robot = TriangularRobot(xp,yp,theta(1));
+        plot(robot(:,1),robot(:,2),'-',xp,yp,'-');
+        xlim([0 200])
+        ylim([0 200])
+        pause(0.01)
+        
+    end    
+end
 plot(time, velData);
 figure;
 plot(time, xData);
